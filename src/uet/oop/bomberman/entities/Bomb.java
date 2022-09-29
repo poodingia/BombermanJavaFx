f5086db74c68a35e482444aa725d000bbd356d05
Phung Minh Tuan Anh <tuananhphungminh1212@gmail.com>
2022-09-29 21:28:39 +0700

    Moving

---
+++ src/uet/oop/bomberman/entities/Bomb.java
@@ -0,0 +1,15 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+
+public class Bomb extends Entity {
+
+    public Bomb(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
