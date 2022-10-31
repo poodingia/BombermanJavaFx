259b442f143fc09f47bc66e76f269ac88c146494
Phung Minh Tuan Anh <tuananhphungminh1212@gmail.com>
2022-10-31 16:30:22 +0700

    Update movement Enemy

--- src/uet/oop/bomberman/entities/character/enemy/Oneal.java
+++ src/uet/oop/bomberman/entities/character/enemy/Oneal.java
@@ -5,6 +5,7 @@ import static uet.oop.bomberman.BombermanGame.characters;
 import javafx.scene.image.Image;
 import uet.oop.bomberman.entities.character.Bomber;
 import uet.oop.bomberman.entities.character.enemy.AI.AIHigh;
+import uet.oop.bomberman.entities.character.enemy.AI.AIMedium;
 import uet.oop.bomberman.graphics.Sprite;
 
 public class Oneal extends Enemy {
@@ -14,7 +15,7 @@ public class Oneal extends Enemy {
         this.points = 200;
         this.speed = 0.5;
         this.deadImage = Sprite.oneal_dead.getFxImage();
-        ai = new AIHigh((Bomber) characters.get(0), this);
+        ai = new AIMedium((Bomber) characters.get(0), this);
         this.direction = -1;
     }
 
